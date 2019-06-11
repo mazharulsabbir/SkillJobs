d3d11447ea5f872bb69696cf18e051a6de53ab93
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-06-12 00:17:44 +0600

    Splash screen design

--- app/src/main/java/skill/jobs/SplashScreen.java
+++ app/src/main/java/skill/jobs/SplashScreen.java
@@ -1,7 +1,10 @@
 package skill.jobs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(
+                        SplashScreen.this,
+                        LoginActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }

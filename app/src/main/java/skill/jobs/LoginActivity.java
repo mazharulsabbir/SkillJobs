6ad3d9b984356b375298a3d7d4148b781bb8ca92
Rs Robin <srrobin33@gmail.com>
2019-06-13 00:49:34 +0600

    Registration Activity Design

--- app/src/main/java/skill/jobs/LoginActivity.java
+++ app/src/main/java/skill/jobs/LoginActivity.java
@@ -2,13 +2,25 @@ package skill.jobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button Regi_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Regi_Button=findViewById(R.id.btRegistration);
+
+        Regi_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
+            }
+        });
     }
 }

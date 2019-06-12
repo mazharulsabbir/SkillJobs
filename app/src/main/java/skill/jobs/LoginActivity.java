eae6b799f007a9c70ecde5b27ece6d62c1c82ed0
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-06-12 23:11:27 +0600

    TabView

--- app/src/main/java/skill/jobs/LoginActivity.java
+++ app/src/main/java/skill/jobs/LoginActivity.java
@@ -1,8 +1,10 @@
 package skill.jobs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void btnSignInClicked(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
---
+++ app/src/main/java/skill/jobs/SectionsPagerAdapter.java
@@ -0,0 +1,25 @@
+package skill.jobs;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}

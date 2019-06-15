0e1cb160030d0f46a17208256f16caa980d529d3
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-06-15 14:29:57 +0600

    Login & Registration: Shared Animation Implementation..

--- app/src/main/java/skill/jobs/LoginActivity.java
+++ app/src/main/java/skill/jobs/LoginActivity.java
@@ -1,13 +1,16 @@
 package skill.jobs;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.StyleSpan;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,7 +50,26 @@ public class LoginActivity extends AppCompatActivity {
         Regi_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                Button btnRegistration = findViewById(R.id.btRegistration);
+                ImageView appLogo = findViewById(R.id.imageView);
+                TextView txtPartner = findViewById(R.id.textView2);
+                TextView txtHeading = findViewById(R.id.txt_heading);
+                TextView textView = findViewById(R.id.textView3);
+
+                Intent sharedIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
+
+                Pair[] pairs = new Pair[5];
+                pairs[0] = new Pair<View, String>(textView, "txt_login_or_register");
+                pairs[1] = new Pair<View, String>(appLogo, "ic_app_logo");
+                pairs[2] = new Pair<View, String>(txtPartner, "txt_partner");
+                pairs[3] = new Pair<View, String>(btnRegistration, "btn_register");
+                pairs[4] = new Pair<View, String>(txtHeading, "txt_headings");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                    startActivity(sharedIntent, options.toBundle());
+
+                }
             }
         });
     }
--- app/src/main/java/skill/jobs/RegistrationActivity.java
+++ app/src/main/java/skill/jobs/RegistrationActivity.java
@@ -1,8 +1,13 @@
 package skill.jobs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        TextView textView = findViewById(R.id.textView3);
+
+        String text = "Login or ";
+
+        SpannableString ss;
+        ss = new SpannableString(text);
+
+        StyleSpan boldSpan;
+        boldSpan = new StyleSpan(Typeface.BOLD);
+        ss.setSpan(boldSpan, 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+
+        text = "Register to explore all our new service. We offer you the best career solution.";
+
+        boldSpan = new StyleSpan(Typeface.BOLD);
+        ss = new SpannableString(text);
+        ss.setSpan(boldSpan, 0, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.append(ss);
+
     }
 }

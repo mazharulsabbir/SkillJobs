bf4be74dec6763442d4fec876347519fd0779c53
Rs Robin <srrobin33@gmail.com>
2019-06-16 19:51:06 +0600

    modify Rgistration Page

--- app/src/main/java/skill/jobs/RegistrationActivity.java
+++ app/src/main/java/skill/jobs/RegistrationActivity.java
@@ -1,27 +1,79 @@
 package skill.jobs;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    TextView textView, suTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        TextView textView = findViewById(R.id.textView3);
+         textView = findViewById(R.id.textView3);
+         suTextView = findViewById(R.id.textView8);
 
+        StringUrlDesgin();
+
+
+    }
+
+    private void StringUrlDesgin() {
         String text = "Login or ";
+        String sutext = "By clicking the button, I agree to the Terms of Service and Privacy Policy.";
 
-        SpannableString ss;
-        ss = new SpannableString(text);
+        SpannableString ss=new SpannableString(text);;
+        SpannableString su=new SpannableString(sutext);
+
+
+        ClickableSpan clickableSpan1=new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Toast.makeText(RegistrationActivity.this, "Terms of Service", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(false);
+            }
+        };
+
+        ClickableSpan clickableSpan2=new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Toast.makeText(RegistrationActivity.this, "Privacy Policy", Toast.LENGTH_SHORT).show();
+
+            }
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(false);
+            }
+        };
+
+
+        su.setSpan(clickableSpan1,39,55,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        su.setSpan(clickableSpan2,60,74,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        suTextView.setText(su);
+        suTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
 
         StyleSpan boldSpan;
         boldSpan = new StyleSpan(Typeface.BOLD);

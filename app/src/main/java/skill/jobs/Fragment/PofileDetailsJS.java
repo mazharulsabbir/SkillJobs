d5c07bb586239f2b6a4fb486b32fc036109eb987
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-07-15 23:37:43 +0600

    Changes On Profile UI

--- app/src/main/java/skill/jobs/Fragment/PofileDetailsJS.java
+++ app/src/main/java/skill/jobs/Fragment/PofileDetailsJS.java
@@ -36,15 +36,8 @@ public class PofileDetailsJS extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.design_job_seeker_profile_ui, container, false);
-//        pro_I_edit = view.findViewById(R.id.js_pro_edit_imaage);
-//        pro_I_edit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ProfileInformationEditJobSeeker();
-//            }
-//        });
+
         mNestedScrollView = view.findViewById(R.id.nestedScrollView);
 
         mRecyclerViewProfileInfo = view.findViewById(R.id.recycler_view);
@@ -62,13 +55,13 @@ public class PofileDetailsJS extends Fragment {
     private void initSampleData() {
         info = new ArrayList<>();
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Lives In", "Dhaka, Bangladesh");
+        helper = new ProfileInformationHelper(R.drawable.ic_my_location_black_24dp, "Lives In", "Dhaka, Bangladesh");
         info.add(0, helper);
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Contact No", "01825632294");
+        helper = new ProfileInformationHelper(R.drawable.ic_phone_black_24dp, "Contact No", "01825632294");
         info.add(1, helper);
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Birth Date", "1999-02-12");
+        helper = new ProfileInformationHelper(R.drawable.ic_date_black_24dp, "Birth Date", "1999-02-12");
         info.add(2, helper);
 
         helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Gender", "Male");
@@ -80,16 +73,16 @@ public class PofileDetailsJS extends Fragment {
         helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Nationality", "Bangladesh");
         info.add(5, helper);
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Expected Salary", "");
+        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Expected Salary", "-");
         info.add(6, helper);
 
         helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "National ID", "1954886790");
         info.add(7, helper);
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Passport No", "");
+        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Passport No", "-");
         info.add(8, helper);
 
-        helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Birth Certificate", "");
+        helper = new ProfileInformationHelper(R.drawable.ic_date_black_24dp, "Birth Certificate", "-");
         info.add(9, helper);
 
         helper = new ProfileInformationHelper(R.drawable.ic_settings_black_24dp, "Present Address", "103 Central Bashabo, Khilgaon, District: Dhaka, Post Code: 1219, Bangladesh");
@@ -119,28 +112,4 @@ public class PofileDetailsJS extends Fragment {
             }
         }, 200);
     }
-
-
-   /* private void ProfileInformationEditJobSeeker() {
-        FrameLayout fr = view.findViewById(R.id.js_profile_edit_container);
-
-        if (pro_edit_flag) {
-            if (pro_edit_open) {
-
-                //profile edit fragment open
-                Fragment fragment = new ProfileEditJobseekerFragment();
-                FragmentManager fm = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.add(R.id.js_profile_edit_container, fragment).commit();
-                pro_edit_open = false;
-            }
-            fr.setVisibility(View.VISIBLE);
-            pro_edit_flag = false;
-        } else {
-            fr.setVisibility(View.GONE);
-            pro_edit_flag = true;
-        }
-
-    }*/
-
 }

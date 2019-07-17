205a1ef9d770a4331cac3db61d545328fa11dc25
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-07-18 00:44:35 +0600

    Job Seeker Profile Objectives

--- app/src/main/java/skill/jobs/MainActivity.java
+++ app/src/main/java/skill/jobs/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 

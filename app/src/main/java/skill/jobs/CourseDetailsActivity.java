73862480fd6cc225fcd798b6607890edac6a95bf
Rs Robin <srrobin33@gmail.com>
2019-07-30 01:25:23 +0600

    api modification

--- app/src/main/java/skill/jobs/CourseDetailsActivity.java
+++ app/src/main/java/skill/jobs/CourseDetailsActivity.java
@@ -2,13 +2,56 @@ package skill.jobs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CourseDetailsActivity extends AppCompatActivity {
+    Intent mintent;
+    private String title,summary,detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+
+        retrivedata();
+        setdata();
+
+
+    }
+
+    private void setdata() {
+        TextView Title=findViewById(R.id.jd_courseTitle);
+        Title.setText(title);
+        TextView Summary=findViewById(R.id.jd_summaryOverview);
+        if (summary.equals("null")){
+            Summary.setVisibility(View.GONE);
+
+        }else {
+
+            Summary.setText(Html.fromHtml(summary));
+        }
+
+
+        TextView Detail=findViewById(R.id.jd_contentOverview);
+        if (detail.equals("null")){
+            Detail.setVisibility(View.GONE);
+        }else {
+            Detail.setText(Html.fromHtml(detail));
+        }
+    }
+
+
+    private void retrivedata(){
+        mintent=getIntent();
+        title=mintent.getStringExtra("Title");
+        summary=mintent.getStringExtra("Summary");
+        detail=mintent.getStringExtra("Detail");
+
     }
 }
--- app/src/main/java/skill/jobs/fragment/RunningCourseFragment.java
+++ app/src/main/java/skill/jobs/fragment/RunningCourseFragment.java
@@ -1,31 +1,51 @@
 package skill.jobs.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+import skill.jobs.CourseDetailsActivity;
 import skill.jobs.R;
+import skill.jobs.database.JsonPlaceHolderApi;
+import skill.jobs.database.RunCourse;
 import skill.jobs.recyclerview.adapter.RunningCourseAdapter;
 import skill.jobs.recyclerview.helper.RunningCourseHelper;
+import skill.jobs.recyclerview.helper.UpcommingCourse;
 
 public class RunningCourseFragment extends Fragment {
     View view;
+
     private List<RunningCourseHelper> RunnigCourses;
 
     private BaseQuickAdapter mRunningCoursesAdapter;
 
     private RecyclerView mRecyclerViewRunningCourse;
+    ArrayList<RunCourse> retroModelArrayList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,20 +53,113 @@ public class RunningCourseFragment extends Fragment {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_running_course, container, false);
 
-        initializecourse();
+
+        ApiData();
         initRecyclerView();
-        RunningCoursesAdapter();
+
 
 
 
         return view;
     }
+    private void ApiData() {
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://training.skill.jobs/api/v1/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+
+        JsonPlaceHolderApi mapi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<String> call = mapi.getUpcourses();
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.i("Response: ", response.body());
+                if (!response.isSuccessful()) return;
+
+                if (response.body() != null) {
+                    Log.i("onSuccess", response.body());
+
+                    String jsonResponse = response.body();
+                    getJobLists(jsonResponse);
+
+                } else {
+                    Log.i("onEmptyResponse", "Returned empty response");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+                Toast.makeText(getContext(), t.getMessage() + "", Toast.LENGTH_SHORT).show();
+                Log.i("Response error: ", t.getMessage());
+            }
+        });
+
+
+    }
+
+    private void getJobLists(String response) {
+        try {
+            //getting the whole json object from the response
+            JSONObject obj = new JSONObject(response);
+            retroModelArrayList= new ArrayList<>();
+            JSONArray dataArray = obj.getJSONArray("data");
+            RunnigCourses = new ArrayList<>();
+
+            for (int i = 0; i < dataArray.length(); i++) {
+                RunCourse runCourse = new RunCourse();
+                JSONObject jsonObject = dataArray.getJSONObject(i);
+
+                runCourse.setName(jsonObject.getString("name"));
+                runCourse.setSummary(jsonObject.getString("summary"));
+                runCourse.setDetail(jsonObject.getString("detail"));
+
+                retroModelArrayList.add(runCourse);
+            }
+
+            for (int j = 0; j < retroModelArrayList.size(); j++) {
+                RunningCourseHelper helper = new RunningCourseHelper(
+                        retroModelArrayList.get(j).getName() + "",
+                        "48 ");
+
+                RunnigCourses.add(helper);
+
+            }
+
+            RunningCoursesAdapter();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     private void RunningCoursesAdapter() {
         mRunningCoursesAdapter=new RunningCourseAdapter(R.layout.design_running_course,RunnigCourses);
         mRunningCoursesAdapter.isFirstOnly(false);
         mRunningCoursesAdapter.openLoadAnimation();
         mRecyclerViewRunningCourse.setAdapter(mRunningCoursesAdapter);
+
+        mRunningCoursesAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View v, int position) {
+
+
+                Intent intent=new Intent(getActivity(), CourseDetailsActivity.class);
+                intent.putExtra("Title",retroModelArrayList.get(position).getName());
+                intent.putExtra("Summary",retroModelArrayList.get(position).getSummary());
+                intent.putExtra("Detail",retroModelArrayList.get(position).getDetail());
+                startActivity(intent);
+
+            }
+        });
 
 
     }
@@ -60,18 +173,5 @@ public class RunningCourseFragment extends Fragment {
         mRecyclerViewRunningCourse.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
-    private void initializecourse() {
-
-
-        RunnigCourses=new ArrayList<>();
-        for(int i=0;i<10;i++){
-            RunningCourseHelper helper=new RunningCourseHelper(
-                    "Title",
-                    "50"
-            );
-            RunnigCourses.add(helper);
-        }
-
-    }
 
 }
